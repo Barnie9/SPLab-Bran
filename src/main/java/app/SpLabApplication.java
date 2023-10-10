ebbc519c215ae09806662b2129ed23183df9901c
Bran Alexandru <bran.alexandru.2002@gmail.com>
2023-10-10 19:08:06 +0300

    Added a custom error controller

--- src/main/java/app/SpLabApplication.java
+++ src/main/java/app/SpLabApplication.java
@@ -1,4 +1,4 @@
-package com.example.splab;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/app/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package app.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseBody
+    String error(HttpServletRequest request) {
+        return "<h1>Error occurred</h1>";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+
+}
